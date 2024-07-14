df5e652e24e6c3a4800b105a046862803245689a
Yannick Richter <git@y-richter.de>
2024-07-14 19:00:09 +0200

    Initial commit

---
+++ Example.py
@@ -0,0 +1,18 @@
+from ba63usb import BA63USB
+
+def test():
+    devs = BA63USB.get()
+    dev = BA63USB(devs[0]["path"])
+
+    dev.clear()
+    dev.set_charset(0x34) # Change to 858 codepage Latin1+€ 
+    dev.set_cursor(1,1)
+    dev.print("Test °äöü€#*\n\rCount")
+
+    for i in range(255):
+        dev.set_cursor(2,7)
+        dev.print(f"{i}".ljust(3))
+    
+
+if __name__ == "__main__":
+    test()
